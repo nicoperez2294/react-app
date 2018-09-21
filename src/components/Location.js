6c37f85e1981e120e6c4f64eef8a7eb9f0e56b48
Nicolas <np@xipron.com>
2018-09-21 02:18:02 -0300

    Se aplican algunos conceptos de components

---
+++ src/components/Location.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Location = ({city}) => {
+    return <h1>{ city }</h1>;
+};
+
+export default Location;
---
+++ src/components/WeatherExtraInfo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WeatherExtraInfo = () => {
+    return <div>Información Extra</div>;
+}
+
+export default WeatherExtraInfo;
